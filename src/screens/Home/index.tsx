5b377fde93b8b2ae4d3812a2d421fee87d5fb30c
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-01 20:13:44 -0300

    feat: add input form.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,28 @@
-import { Text, View } from 'react-native';
-import { styles } from './styles';
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar!");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
+
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2025.</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
