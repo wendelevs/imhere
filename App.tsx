b577014b27aa1d43b6a94f63314300844540d429
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-01 19:41:00 -0300

    refactor: The home code was separated.
    The home code was separated from App into another folder to improve organization.

--- App.tsx
+++ App.tsx
@@ -1,29 +1,7 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Home } from './src/screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2025.</Text>
-    </View>
+    <Home />
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#131016",
-    padding: 24,
-  },
-
-  eventName: {
-    color: "#FDFCFE",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 48,
-  },
-
-  eventDate: {
-    color: "#6B6B6B",
-    fontSize: 16,
-  },
-});
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2025.</Text>
+    </View>
+  );
+}
