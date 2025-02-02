7cf4640c19ec2eb9d2b07f207ad02ac6cd18e8b8
wendelevs <107012134+wendelevs@users.noreply.github.com>
2025-02-02 16:40:13 -0300

    feat: add function to remove users.

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -30,7 +30,7 @@ export function Home() {
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado!"),
+        onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name)),
       },
       {
         text: "Não",
